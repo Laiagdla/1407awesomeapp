c232f0711fabbff43efee938d8b118c66609a7fb
Laia Grobe <laia.gdla@gmail.com>
2024-02-17 09:52:14 +0100

    escential

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if "git+" not in x]
+
+setup(name='awesomeapp',
+      description="example 1407 batch package",
+      license="MIT",
+      version=0.1,
+      author="me",
+      install_requires=requirements,
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
